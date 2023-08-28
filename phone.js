4c0b784f5c93319d6387c0bb0c35002ff289bcf0
Arifice <arifice13@gmail.com>
2023-08-28 21:07:45 +0600

    loadding button added

--- phone.js
+++ phone.js
@@ -9,9 +9,22 @@ const loadPhone= async(searchText)=>{
 const displayPhones=phones=>{    
     // step-1: get container 
     const phoneContainer=document.getElementById('phone-container');
+    const showAllContainer=document.getElementById('show all-container');
+    // display show all button
+        console.log(phones,phones.length);
+        const length=phones.length;
+        if(length>12){
+            showAllContainer.classList.remove('hidden');
+        }
+        else{
+            showAllContainer.classList.add('hidden');
+        }
+    // display only first 12 phone
+    phones=phones.slice(0,12);
     // clear container
     phoneContainer.textContent='';
-    phones.forEach(phone=>{        
+    phones.forEach(phone=>{  
+        console.log(phone)      
         // step-2: Create a div
         const phoneCaard=document.createElement('div');
         // step-3: Add class
@@ -20,8 +33,9 @@ const displayPhones=phones=>{
         phoneCaard.innerHTML=`
             <figure><img src="${phone.image}"></figure>
             <div class="card-body">
+                <h1 class="card-title text-2xl font-[600]">Brand: ${phone.brand}</h1>
                 <h1 class="card-title text-4xl font-[700]">${phone.phone_name}</h1>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <p class="text-2xl">${phone.slug}</p>
                 <div class="card-actions justify-end">
                     <button class="btn btn-primary">Buy Now</button>
                 </div>
@@ -30,14 +44,32 @@ const displayPhones=phones=>{
         // step-5:Appened child
         phoneContainer.appendChild(phoneCaard);
     })
+    toggoleLoadingDots(false);
 
 }
 // search button handle
 const searchButton=()=>{
+    toggoleLoadingDots(true);
     const searchField=document.getElementById('search-field');
     const searchText=searchField.value;
     loadPhone(searchText);
 
 }
+// handle searce recap
+// const searchButton2=()=>{
+//     toggoleLoadingDots(true);
+//     const searchField2=document.getElementById('search-field2');
+//     const searchText2=searchField2.value;
+//     loadPhone(searchText2);
+// }
 
-loadPhone();
+const toggoleLoadingDots=(isLoading)=>{
+    const loadingDots=document.getElementById('loading-dots');
+    if(isLoading){
+        loadingDots.classList.remove('hidden');
+    }
+    else{
+        loadingDots.classList.add('hidden');
+    }
+}
+// loadPhone();
